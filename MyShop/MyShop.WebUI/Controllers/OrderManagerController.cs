cfaebc496f54f800df0f12a18f556c8635d9f94f
xtugaz <62811106+xtugaz@users.noreply.github.com>
2021-01-23 15:12:32 +0000

    Added Order Manager

---
+++ MyShop/MyShop.WebUI/Controllers/OrderManagerController.cs
@@ -0,0 +1,53 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        IOrderService orderService;
+
+        public OrderManagerController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+        // GET: OrderManager
+        public ActionResult Index()
+        {
+            List<Order> orders = orderService.GetOrderList();
+
+            return View(orders);
+        }
+
+        public ActionResult UpdateOrder(string Id)
+        {
+            ViewBag.StatusList = new List<string>()
+            {
+                "Order Created",
+                "Payment Processed",
+                "Order Shipped",
+                "Order Complete"
+            };
+
+            Order order = orderService.GetOrder(Id);
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateOrder(Order updatedOrder, string Id)
+        {
+            Order orderToUpdate = orderService.GetOrder(Id);
+
+            orderToUpdate.OrderStatus = updatedOrder.OrderStatus;
+            orderService.UpdateOrder(orderToUpdate);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
