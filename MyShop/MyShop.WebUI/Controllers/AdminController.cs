80e8a09fe1b06d79cbd2b9752697dcec4710550a
xtugaz <62811106+xtugaz@users.noreply.github.com>
2021-01-23 15:43:58 +0000

    Secured the admin pages

--- MyShop/MyShop.WebUI/Controllers/AdminController.cs
+++ MyShop/MyShop.WebUI/Controllers/AdminController.cs
@@ -6,6 +6,10 @@ using System.Web.Mvc;
 
 namespace MyShop.WebUI.Controllers
 {
+    //[Authorize(Users = "admin@mywebsite.com")] Add Specific Users as Admins
+    [Authorize(Roles ="Admin")] //Specify the type of Role necessary to access that necessary must be created and set in DataBase
+    //The other way is to use ClaimTypes specified by Microsoft. Creating a ClaimType with get URL from ClaimType Role and create 
+    //with ClaimValue Admin in Database for our User
     public class AdminController : Controller
     {
         // GET: Admin
